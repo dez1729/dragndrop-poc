c2cefe9a41c71c5ce4182c3ac8f1faa6e8acb17a
dleung <thedes+git@gmail.com>
2020-05-17 19:59:49 -0700

    started playing with drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import {CdkDragDrop, moveItemInArray, CdkDragStart, CdkDragEnd} from '@angular/cdk/drag-drop';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,101 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dnd-poc';
+  draggingData = [];
+  lastClonedDraggingData = [];
+
+  data = [
+    {
+      index: 0,
+      displayIndex: 1,
+      textField: "The Phantom Menace",
+      groupedIndices: [0, 1],
+    },
+    {
+      index: 1,
+      displayIndex: 2,
+      textField: "The Attack of the Clones",
+      groupedIndices: [0, 1],
+    },
+    {
+      index: 2,
+      displayIndex: 3,
+      textField: "The Revenge of the Sith",
+      groupedIndices: [2],
+    },
+    {
+      index: 3,
+      displayIndex: 4,
+      textField: "A New Hope",
+      groupedIndices: [3],
+    },
+    {
+      index: 4,
+      displayIndex: 5,
+      textField: "The Empire Strikes Back",
+      groupedIndices: [4, 5, 6],
+    },
+    {
+      index: 5,
+      displayIndex: 6,
+      textField: "The Return of the Jedi",
+      groupedIndices: [4, 5, 6],
+    },
+    {
+      index: 6,
+      displayIndex: 7,
+      textField: "The Force Awakens",
+      groupedIndices: [4, 5, 6],
+    },
+    {
+      index: 7,
+      displayIndex: 8,
+      textField: "The Last Jedi",
+      groupedIndices: [7, 8],
+    },
+    {
+      index: 8,
+      displayIndex: 9,
+      textField: "The Rise of Skywalker",
+      groupedIndices: [7, 8],
+    },
+  ];
+
+  // outputData$: Observable<any> = of(this.data);
+
+  constructor() {
+
+  }
+
+  drop(event: CdkDragDrop<any[]>) {
+    console.log(event);
+    // console.log(_.get(event, ['source', 'data']));
+    // moveItemInArray(this.data, event.previousIndex, event.currentIndex);
+    // this.data.splice(event.previousIndex - 1, 0);
+    // this.data.splice.apply(this.data, [event.currentIndex, 0].concat(_.concat(this.draggingData, this.lastClonedDraggingData)));
+    // this.draggingData = [];
+
+
+    this.lastClonedDraggingData = [];
+  }
+
+  dragStarted(event: CdkDragStart<any>, currentIndex: number, groupedIndices: number[]) {
+    console.log(event);
+    // this.lastClonedDraggingData = _.cloneDeep(this.data[groupedIndices[groupedIndices.length - 1]]);
+    // this.draggingData = this.data.splice(groupedIndices[0], groupedIndices.length - 1);
+    // [this.draggingData, this.data] = _.partition(this.data, v => _.includes(this.data[currentIndex].groupedIndices, v.index));
+    this.draggingData = _.filter(this.data, v => _.includes(this.data[currentIndex].groupedIndices, v.index));
+    _.remove(this.data, v => _.includes(_.filter(groupedIndices, u => !_.eq(u, currentIndex)), v.index));
+    console.log(this.draggingData);
+    console.log(this.data);
+  }
+
+  dragEnded(event: CdkDragEnd<any>, currentIndex: number, groupedIndices: number[]) {
+    console.log(event);
+    // this.data.splice.apply(this.data, [currentIndex, 0].concat(this.draggingData));
+    // this.draggingData = [];
+    console.log(this.data);
+    // document.getElementById('exampleBox').style.cursor = "default";
+    // event.source.element.nativeElement.style.cursor = "";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatTableModule } from '@angular/material/table';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { PortalModule } from '@angular/cdk/portal';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    DragDropModule,
+    PortalModule,
+    MatCardModule,
+    MatButtonModule,
+    MatProgressBarModule,
+    OverlayModule,
+    MatTableModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
